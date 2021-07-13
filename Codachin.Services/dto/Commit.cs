ee5b2823edbe5299ca9dde6ef62d312363d2dcbf
Pandinni <potbanda@protonmail.com>
2021-07-13 02:57:21 +0100

    Removed non used dependency Cake.powershell

--- Codachin.Services/dto/Commit.cs
+++ Codachin.Services/dto/Commit.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Management.Automation;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.IO;
 
 namespace Codachin.Services.dto
 {
